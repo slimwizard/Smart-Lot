30351a5e78ad3ea65676ae5fa669339ca867f70e
slimwizard <mrice@Matthews-MacBook-Pro-2.local>
2019-02-04 10:01:23 -0600

    updating client to use angular 7

--- client/client-app/deploy-to-aws.bat
+++
@@ -1,3 +0,0 @@
-:: This script builds and deploys the Angular 2 project to an s3 bucket titled "smart-lot.io"
-ng build --prod --aot
-aws s3 cp ./dist s3://smart-lot.io --recursive --acl public-read
