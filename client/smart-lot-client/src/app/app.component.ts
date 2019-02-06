e61894d8126ae68c8f473e5d0d9a3b9a6e4c8320
slimwizard <mrice@Matthews-MacBook-Pro-2.local>
2019-02-05 22:50:54 -0600

    added material design package

--- client/smart-lot-client/src/app/app.component.ts
+++ client/smart-lot-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatExpansionModule, matExpansionAnimations} from '@angular/material';
 
 @Component({
   selector: 'app-root',
--- client/smart-lot-client/src/app/app.module.ts
+++ client/smart-lot-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatExpansionModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
