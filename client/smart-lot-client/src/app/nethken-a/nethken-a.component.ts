ef9b34031ef239b7db9cf4a6ebc0165eace22913
slimwizard <mrice@Matthews-MacBook-Pro-2.local>
2019-02-11 21:40:11 -0600

    added refresh buttons, icons, and timer for spinner

--- client/smart-lot-client/src/app/nethken-a/nethken-a.component.ts
+++ client/smart-lot-client/src/app/nethken-a/nethken-a.component.ts
@@ -2,15 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { MatCardModule, MatButtonModule, MatProgressSpinner } from '@angular/material'
 import { ParkingSpot, LotAvailabilityService } from '../services/lot-availability.service';
 
+
 @Component({
   selector: 'app-nethken-a',
   templateUrl: './nethken-a.component.html',
   styleUrls: ['./nethken-a.component.css']
 })
+
 export class NethkenAComponent implements OnInit {
 
   constructor(private lotAvailibilityService: LotAvailabilityService) { }
-  parkingSpots: ParkingSpot[]
   occupiedSpots;
 
   isLoading: boolean = true; 
@@ -18,19 +19,19 @@ export class NethkenAComponent implements OnInit {
   mode = 'indeterminate';
   value = 50;
   
-
   isOccupied(spotNumber: number): boolean {
     return this.occupiedSpots.indexOf(spotNumber) != -1
   }
 
-  ngOnInit() {
+  getLotAvailibility(): void {
     this.lotAvailibilityService.getLotData("NethkenA").subscribe(data => {
       this.isLoading = true;
-      this.parkingSpots = data
-      this.occupiedSpots = this.parkingSpots.filter(item => item.occupied == true).map(item => item.spot_number)
-      this.isLoading = false;
+      this.occupiedSpots = data.filter(item => item.occupied == true).map(item => item.spot_number)
+      setTimeout(() => {this.isLoading=false}, 1000)
     })
-    
   }
 
+  ngOnInit() {
+    this.getLotAvailibility()
+  }
 }
