8adf64db3e9195c8dba8f53a89752981c180d653
Addison Abercrombie <aha028@latech.edu>
2019-02-14 20:10:41 -0600

    added functionality to test grabbing user location

--- client/smart-lot-client/src/app/app.component.ts
+++ client/smart-lot-client/src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SMART LOT';
+  currentLat: any;
+  currentLong: any;
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.title = this.currentLat;
+  }
+
 }
+
+
