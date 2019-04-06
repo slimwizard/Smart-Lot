865d4c7bf241d069182633f2053909ff34350c46
Grant <g.free697@gmail.com>
2019-04-06 18:40:18 -0500

    adding get_lot test, removing unused functions, refactoring test script

--- lot-availability-api/application.py
+++ lot-availability-api/application.py
@@ -32,22 +32,12 @@ application.config['UPLOAD_FOLDER'] = str(UPLOAD_FOLDER)
 
 db = SQLAlchemy(application)
 
+# index endpoint for smoke testing
 @application.route('/')
 def index():
     return "Hewwo wowwd"
 
-@application.route('/smart-lot/lots/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return "No file"
-    file = request.files['file']
-    file.save("static/test.jpg")
-    return "Saved successfully"
-
-@application.route('/smart-lot/lots', methods=['GET'])
-def get_tasks():
-    return jsonify({'lots': lots})
-
+# gets all spots in a lot by id
 @application.route('/smart-lot/lots/<id>', methods=['GET'])
 def get_lot(id):
     lot_info = db.session.query(Spots).filter_by(lot_id=id)
@@ -58,7 +48,7 @@ def get_lot(id):
         abort(404)
     response = jsonify(rows)
     response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return response, 200
 
 @application.route('/smart-lot/lots/by_location/<string:lat_long>', methods=['GET'])
 def get_lots_by_location(lat_long):
