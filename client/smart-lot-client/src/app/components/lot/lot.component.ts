0c82cd9fd4d37c46daa7aeada9c8b7a300719f80
Matthew LaCroix <mbl017@latech.edu>
2019-05-07 17:36:35 -0500

    changing polling api to https

--- client/smart-lot-client/src/app/components/lot/lot.component.ts
+++ client/smart-lot-client/src/app/components/lot/lot.component.ts
@@ -129,7 +129,7 @@ export class LotComponent implements OnInit, OnDestroy {
     this.getLotInfo()
 	this.getLotAvailibility();
 	const request$ = ajax({
-        url: 'api.smart-lot.io/smart-lot/lots/polling/' + this.current_UUID,
+        url: 'https://api.smart-lot.io/smart-lot/lots/polling/' + this.current_UUID,
         //url: 'http://127.0.0.1:5000/smart-lot/lots/polling/' + this.current_UUID,
 		crossDomain: true
 	}).pipe(
